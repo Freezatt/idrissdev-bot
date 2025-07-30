2d2439ef62d04b94e8d54d386c71cb50423ede56
Freezatt <idrisshalihadjali@gmail.com>
2025-07-30 16:23:46 +0100

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,6 +1,6 @@
 import requests
 
-URL = "https://رابط_موقعك.replit.dev/"
+URL = "https://1b5a6a61-5ca8-48cc-b9e0-fe2887f73697-00-16k0sfg61afq7.janeway.replit.dev/"
 
 try:
     response = requests.get(URL)
