81ade5fc86a5d363ae71b321ab6ffebe7fb8d6d8
Freezatt <idrisshalihadjali@gmail.com>
2025-07-30 16:16:10 +0100

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,1 +1,9 @@
+import requests
 
+URL = "https://رابط_موقعك.replit.dev/"
+
+try:
+    response = requests.get(URL)
+    print("✅ الموقع تم زيارته بنجاح!")
+except Exception as e:
+    print(f"❌ فشل في الاتصال: {e}")
